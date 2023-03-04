51d612b112fee874eb565ceda86996e98f98220c
MacB <mac@macbudkowski.com>
2023-03-04 15:13:26 -0700

    Added claim boxes in AlreadyClaimed + Denied pages

--- frontend/pages/AlreadyClaimed.jsx
+++ frontend/pages/AlreadyClaimed.jsx
@@ -1,5 +1,6 @@
 import styles from "../styles/InstructionsComponent.module.css";
 import Router, { useRouter } from "next/router";
+import ClaimFundsForm from '../components/ClaimFundsForm.jsx';
 export default function InstructionsComponent() {
 	const router = useRouter();
 	return (
@@ -10,7 +11,12 @@ export default function InstructionsComponent() {
 				</h1>
 			</header>
 			<p>{router.query.address} has already claimed GoerliETH and is not eligible for another claim.</p>
-	
+			<br />
+			<br />
+			<br />
+			<br />
+			<p>Try another address</p>
+			<ClaimFundsForm />
 		</div>
 	);
 }
